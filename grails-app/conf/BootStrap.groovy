452738feaa55e1d409ccb5361b8cb3915e9c24c2
YaroslavTir <yaroslavTir@gmail.com>
2013-10-13 20:56:27 +0400

    4.4 Bootstrapping reference data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,29 @@
-class BootStrap {
+import com.grailsinaction.Profile
+import com.grailsinaction.User
+import grails.util.Environment
 
+class BootStrap {
     def init = { servletContext ->
+        switch (Environment.current) {
+            case Environment.DEVELOPMENT:
+                createAdminUserIfRequired()
+                break;
+            case Environment.PRODUCTION:
+                println "No special configuration required"
+                break;
+        }
     }
     def destroy = {
+    }
+
+    void createAdminUserIfRequired() {
+        if (!User.findByUserId("admin")) {
+            println "Fresh Database. Creating ADMIN user."
+            def profile = new Profile(email: "admin@yourhost.com")
+            def user = new User(userId: "admin",
+                    password: "secret", profile: profile).save()
+        } else {
+            println "Existing admin user, skipping creation"
+        }
     }
 }
